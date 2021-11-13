7aac603f6dd52d1817b05d982a283538ccd157dc
yyhyap <yongheng98@hotmail.com>
2021-11-13 19:05:31 +0800

    Added Beer repository

---
+++ src/main/java/com/yyh/msscbeerservice/repositories/BeerRepository.java
@@ -0,0 +1,9 @@
+package com.yyh.msscbeerservice.repositories;
+
+import com.yyh.msscbeerservice.domain.Beer;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.UUID;
+
+public interface BeerRepository extends JpaRepository<Beer, UUID> {
+}
