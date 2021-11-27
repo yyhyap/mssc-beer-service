f1e9b713b9b6ed88b6c121800c3205ae60fa94e0
yyhyap <yongheng98@hotmail.com>
2021-11-27 13:19:51 +0800

    Configure local MYSQL

--- src/main/java/com/yyh/msscbeerservice/domain/Beer.java
+++ src/main/java/com/yyh/msscbeerservice/domain/Beer.java
@@ -24,7 +24,7 @@ public class Beer {
     @GenericGenerator(name = "UUID", strategy = "org.hibernate.id.UUIDGenerator")
     // mapping to varchar when communicating ith database
     @Type(type="org.hibernate.type.UUIDCharType")
-    @Column(length = 36, columnDefinition = "varchar", updatable = false, nullable = false)
+    @Column(length = 36, columnDefinition = "varchar(36)", updatable = false, nullable = false)
     private UUID id;
 
     @Version
